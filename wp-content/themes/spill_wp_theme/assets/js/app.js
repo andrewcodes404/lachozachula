925f9e1e94fe2f8926a19ead195e9497175947be
Andrew Dickens <andrewcodes404@gmail.com>
2023-01-23 09:46:55 +0000

    remove unused themes, add vscode prefs

--- wp-content/themes/spill_wp_theme/assets/js/app.js
+++ wp-content/themes/spill_wp_theme/assets/js/app.js
@@ -1,84 +1,82 @@
-jQuery( document ).ready( function( $ ) {
-    // $() will work as an alias for jQuery() inside of this function
-    //your code goes here
+jQuery(document).ready(function ($) {
+  // $() will work as an alias for jQuery() inside of this function
+  //your code goes here
 
-///DropDown
+  ///DropDown
+
+  console.log('🌕 ooh');
 
 
-
-$(".menu-item-has-children").hover(function(){
-    console.log("hell ya");
-    // $(this).css("background-color", "red");
+  $(".menu-item-has-children").hover(function () {
     $(this).children('ul').slideToggle();
-    // $(".sub-menu").slideToggle();
-})
+  })
 
 
 
 
 
 
-//////// 👇 👇 👇 👇  OLD OLD OLD 👇 👇 👇 👇 ///////////////
+  //////// 👇 👇 👇 👇  OLD OLD OLD 👇 👇 👇 👇 ///////////////
 
-$(".drop-down-cont").hide();
+  $(".drop-down-cont").hide();
 
-$("#menu-btn").click(function(){
+  $("#menu-btn").click(function () {
 
     $(".drop-down-cont").slideToggle();
-    $(".drop-down-cont").mouseleave(function() {
-            $(".drop-down-cont").fadeOut();
+    $(".drop-down-cont").mouseleave(function () {
+      $(".drop-down-cont").fadeOut();
     })
-})
+  })
 
-$(".drop-menu").hide();
+  $(".drop-menu").hide();
 
-$("#drop-menu-btn").click(function(){
-  $(".drop-menu").slideToggle('fast');
-  $(".drop-menu").mouseleave(function() {
-          $(".drop-menu").fadeOut();
-        })
-})
+  $("#drop-menu-btn").click(function () {
+    $(".drop-menu").slideToggle('fast');
+    $(".drop-menu").mouseleave(function () {
+      $(".drop-menu").fadeOut();
+    })
+  })
 
 
-////tours
-$(".tour-info").hide();
-$(".tour-btn").click(function(){
-//    $(".tour-info").not(this).slideUp();
+  ////tours
+  $(".tour-info").hide();
+  $(".tour-btn").click(function () {
+    //    $(".tour-info").not(this).slideUp();
     $(".tour-info", this).slideToggle();
-//    $(this, ".tour-btn").off();
-//    $(".tour-info").not(this).on();
-})
+    //    $(this, ".tour-btn").off();
+    //    $(".tour-info").not(this).on();
+  })
 
 
-////shop
-$(".shop-item").hide();
+  ////shop
+  $(".shop-item").hide();
 
-$(".shop-btn").click(function(){
+  $(".shop-btn").click(function () {
     $(".shop-item", this).slideToggle();
-})
+  })
 
 
-////////Volunteer
-$("#vol-info").hide();
-$("#vol-btn").click(function(){
+  ////////Volunteer
+  $("#vol-info").hide();
+  $("#vol-btn").click(function () {
     $("#vol-info").slideToggle();
-})
+  })
 
 
-$("#internship").hide();
-$("#intern-btn").click(function(){
+  $("#internship").hide();
+  $("#intern-btn").click(function () {
     $("#internship").slideToggle();
-})
+  })
 
 
-//browser-template
-$("#safari").hide();
+  //browser-template
+  $("#safari").hide();
 
-$("#exchange-btn").click(function () {
+  $("#exchange-btn").click(function () {
     $("#chrome").toggle('slow');
     $("#safari").toggle('slow');
+  });
+
+
 });
-
-
-} );
 ///doc readey end
--- wp-content/themes/spill_wp_theme/template-parts/nav-bar.php
+++ wp-content/themes/spill_wp_theme/template-parts/nav-bar.php
@@ -1,30 +1,64 @@
+<div class="relative w1300">
+  <div class="flex-row nav-bar">
 
-<!-- keep this div or things will break ??-->
-<div>
-
-  <div class="nav-cont">
-
-    <div class="logo">
+    <div class="nav-logo-cont zero-12 sml-12 med-5 lrg-6 flex-col ">
       <a href="<?php echo get_home_url(); ?>">
-          <img class="hover" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/graphics/nav-logo.svg">
+        <img class="hover" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/graphics/nav-logo.svg">
       </a>
     </div>
 
-    <div class="nav-bar ">
-     <?php
-          $defaults = array (
-              ///ask if you want wp to create a container
-              'container' => false,
-              ///telling wp this is where the menu is
-              'theme_location' => 'drop-menu1',
-              ///gives the menu a class for your css
-              'menu_class' => 'nav-drop',
-              'items_wrap' => '%3$s'
-          );
-       /// here's the function to call it
-      wp_nav_menu( $defaults);
+    <!-- <div class="flex-col justify-center"> -->
+
+    <div class="nav-btn-cont zero-12 sml-12 med-7 lrg-6 flex-row  align-center pr1 ">
+
+      <a href="/lcc/about">
+        <div class="nav-btn p05 flex-col justify-center">
+          <p class="hover-scale">about</p>
+        </div>
+      </a>
+
+      <a href="/lcc/shop">
+        <div class="nav-btn p05 flex-col justify-center">
+          <p class="hover-scale">shop</p>
+        </div>
+      </a>
+
+      <a href="/lcc/tour">
+        <div class="nav-btn p05 flex-col justify-center">
+          <p class="hover-scale">tour</p>
+        </div>
+      </a>
+
+      <div class="nav-btn p05 flex-col justify-center">
+        <p id="drop-menu-btn" class="hover-scale">menu</p>
+      </div>
+
+
+      <div class="translate-btn p05 flex-col justify-center my-test">
+        <?php echo do_shortcode('[google-translator]'); ?>
+
+
+      </div>
+    </div>
+
+
+    <!-- </div> -->
+
+    <ul class="the-splodge drop-menu">
+      <?php
+      $defaults = array(
+        ///ask if you want wp to create a container
+        'container' => false,
+        ///telling wp this is where the menu is
+        'theme_location' => 'drop-menu1',
+        ///gives the menu a class for your css
+        'menu_class' => 'nav-drop',
+        'items_wrap' => '%3$s'
+      );
+      /// here's the function to call it
+      wp_nav_menu($defaults);
       ?>
-   
-    </div><!-- .nav-bar -->
+    </ul>
+
   </div>
 </div>
